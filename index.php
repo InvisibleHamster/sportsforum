aae990cc19517a5e8e0454e122d32d7a909eca87
InvisibleHamster <InvisibleHamster@users.noreply.github.com>
2017-05-18 11:29:21 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	include ('layout_manager.php');
+	include ('content_function.php');
+?>
+<html>
+<head><title>Discuss about sports</title></head>
+<link href="/forum/styles/main.css" type="text/css" rel="stylesheet" />
+<body>
+	<div class="pane">
+		<div class="header"><h1><a href="/forum">Sports Forum</a></h1></div>
+		<div class="loginpane">
+			<?php
+				session_start();
+				if (isset($_SESSION['username'])) {
+					logout();
+				} else {
+					if (isset($_GET['status'])) {
+						if ($_GET['status'] == 'reg_success') {
+							echo "<h1 style='color: green;'>new user registered successfully!</h1>";
+						} else if ($_GET['status'] == 'login_fail') {
+							echo "<h1 style='color: red;'>invalid username and/or password!</h1>";
+						}
+					}
+					loginform();
+				}
+			?>
+		</div>
+		<div class="forumdesc">
+			<p>Welcome to the Sports Forum!</p>
+		</div>
+		<div class="content">
+			<?php dispcategories(); ?>
+		</div>
+	</div>
+</body>
+</html>
